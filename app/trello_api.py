073ec740d2abd3f8aa478e747ac78d82055f0b35
alexdraiuk <alexdrajuk@gmail.com>
2020-06-19 18:23:35 +0300

    Update Trello API

--- app/trello_api.py
+++ app/trello_api.py
@@ -1,6 +1,9 @@
+from datetime import datetime as dt
+
 import requests
 
 import settings
+import utils
 
 
 class TrelloAPI:
@@ -18,7 +21,7 @@ class TrelloAPI:
             'token': self.api_token,
             'fields': 'name',
         }
-        response = requests.get(url, headers=headers, params=query)
+        response = requests.get(url, headers=headers, params=query, timeout=settings.REQUESTS_TIMEOUT)
         response.raise_for_status()
         return response.json()
 
@@ -36,7 +39,7 @@ class TrelloAPI:
             'key': self.api_key,
             'token': self.api_token,
         }
-        response = requests.get(url, headers=headers, params=query)
+        response = requests.get(url, headers=headers, params=query, timeout=settings.REQUESTS_TIMEOUT)
         response.raise_for_status()
         return response.json()
 
@@ -56,7 +59,22 @@ class TrelloAPI:
             'key': self.api_key,
             'token': self.api_token,
         }
-        response = requests.get(url, headers=headers, params=query)
+        response = requests.get(url, headers=headers, params=query, timeout=settings.REQUESTS_TIMEOUT)
         response.raise_for_status()
-        cards = [card for card in response.json() if card.get('type') == 'commentCard']
-        return cards[limit:]
+        comments = [action for action in response.json() if action.get('type') == 'commentCard']
+        return comments[:limit]
+
+    def save_initial_comment_date(self, card_id):
+        last_comment = self.get_card_comments(card_id)[0]
+        utils.save_value('last_comment_date', last_comment.get('date'))
+
+    def fetch_new_comments(self, board_name, card_name):
+        card = self.get_card_on_board_by_name(board_name, card_name)
+        comments = self.get_card_comments(card.get('id'))
+
+        last_comment_date = dt.strptime(utils.load_value('last_comment_date'), '%Y-%m-%dT%H:%M:%S.%fZ')
+        new_comments = [
+            comment for comment in comments if
+            dt.strptime(comment.get('date'), '%Y-%m-%dT%H:%M:%S.%fZ') > last_comment_date
+        ]
+        return new_comments
